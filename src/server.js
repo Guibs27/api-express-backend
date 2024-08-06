1c3c3a94b5d089133756c031648e7a8936b81368
Guilherme Byz <gbizerradesouza@gmail.com>
2024-08-06 16:01:52 -0300

    add lib express, .gitigore, script "dev"

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('Hello world!')
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json(
+        {
+            message: "Olá mundo Express!",
+            success: "Sucesso ao executar API"
+        }
+    );
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
