590e594f53977b014edc80495eec14c3bd8e84d6
Guilherme Byz <gbizerradesouza@gmail.com>
2024-08-06 17:45:52 -0300

    add routes: auth & account

--- src/server.js
+++ src/server.js
@@ -1,15 +1,39 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json(
-        {
-            message: "Olá mundo Express!",
-            success: "Sucesso ao executar API"
-        }
-    );
+app.post('/auth/signup', (req, res) => {
+    res.json({message: "Rota de POST auth/signup"});
+});
+
+app.post('/auth/login', (req, res) => {
+    res.json({message: "Rota de POST auth/login"});
+});
+
+app.post('/auth/logout', (req, res) => {
+    res.json({message: "Rota de POST auth/logout"});
+});
+
+
+app.post('/account', (req, res) => {
+    res.json({message: "Rota de POST account"});
+});
+
+app.get('/account/list', (req, res) => {
+    res.json({message: "Rota de GET account list"});
+});
+
+app.get('/account/:id', (req, res) => {
+    res.json({message: "Rota de GET account ID"});
+});
+
+app.put('/account/:id', (req, res) => {
+    res.json({message: "Rota de PUT account"});
+});
+
+app.delete('/account/:id', (req, res) => {
+    res.json({message: "Rota de DELETE account"});
 });
 
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000')
-})
+});
