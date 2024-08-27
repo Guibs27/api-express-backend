fbd68368389ce0a594973ff1ffc04edae8493817
Guilherme Byz <gbizerradesouza@gmail.com>
2024-08-27 17:26:57 -0300

    add prisma, list & getById account

--- src/controllers/account/searchController.js
+++ src/controllers/account/searchController.js
@@ -1,5 +1,18 @@
-const search = (req, res) => {
-    res.json({message: "Rota de GET account ID"});
+import { getByIdAccount } from "../../models/accountModel.js";
+
+const search = async (req, res) => {
+  // const id = req.params.id
+  const { id } = req.params
+  const account = await getByIdAccount(+id)
+
+  if(!account)
+    return res.status(404).json({
+      error: `Conta com ID ${id} não encontrado.`
+    })
+
+  res.json({sucess: "Conta encontrada com sucesso.",
+    account
+  });
 }
 
 export default search
